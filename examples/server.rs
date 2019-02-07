20e4f18a347a39f1095b13e0c5b80591114c35f0
Eliza Weisman <eliza@buoyant.io>
2019-02-07 14:04:27 -0800

    Instrument example server

--- examples/server.rs
+++ examples/server.rs
@@ -5,6 +5,11 @@ extern crate h2;
 extern crate http;
 extern crate tokio;
 
+#[macro_use]
+extern crate tokio_trace;
+extern crate tokio_trace_log;
+extern crate tokio_trace_futures;
+
 use h2::server;
 
 use bytes::*;
@@ -13,57 +18,71 @@ use http::*;
 
 use tokio::net::TcpListener;
 
+use tokio_trace_futures::WithSubscriber;
+
 pub fn main() {
     let _ = env_logger::try_init();
+    let subscriber = tokio_trace_log::TraceLogger::builder()
+        .with_span_entry(true)
+        .with_span_exits(true)
+        .with_span_closes(true)
+        .with_parent_fields(true)
+        .finish();
+    let subscriber = tokio_trace::Dispatch::new(subscriber);
 
-    let listener = TcpListener::bind(&"127.0.0.1:5928".parse().unwrap()).unwrap();
+    tokio_trace::dispatcher::with_default(subscriber.clone(), || {
+        let subscriber2 = subscriber.clone();
+        let listener = TcpListener::bind(&"127.0.0.1:5928".parse().unwrap()).unwrap();
 
-    println!("listening on {:?}", listener.local_addr());
+        info!("listening on {:?}", listener.local_addr());
 
-    let server = listener.incoming().for_each(move |socket| {
-        // let socket = io_dump::Dump::to_stdout(socket);
+        let server = listener.incoming().for_each(move |socket| {
+            // let socket = io_dump::Dump::to_stdout(socket);
 
-        let connection = server::handshake(socket)
-            .and_then(|conn| {
-                println!("H2 connection bound");
+            let connection = server::handshake(socket)
+                .and_then(|conn| {
+                    println!("H2 connection bound");
 
-                conn.for_each(|(request, mut respond)| {
-                    println!("GOT request: {:?}", request);
+                    conn.for_each(|(request, mut respond)| {
+                        info!("GOT request: {:?}", request);
 
-                    let response = Response::builder().status(StatusCode::OK).body(()).unwrap();
+                        let response = Response::builder().status(StatusCode::OK).body(()).unwrap();
 
-                    let mut send = match respond.send_response(response, false) {
-                        Ok(send) => send,
-                        Err(e) => {
-                            println!(" error respond; err={:?}", e);
-                            return Ok(());
+                        let mut send = match respond.send_response(response, false) {
+                            Ok(send) => send,
+                            Err(e) => {
+                                error!(" error respond; err={:?}", e);
+                                return Ok(());
+                            }
+                        };
+
+                        println!(">>>> sending data");
+                        if let Err(e) = send.send_data(Bytes::from_static(b"hello world"), true) {
+                            info!("  -> err={:?}", e);
                         }
-                    };
 
-                    println!(">>>> sending data");
-                    if let Err(e) = send.send_data(Bytes::from_static(b"hello world"), true) {
-                        println!("  -> err={:?}", e);
+                        Ok(())
+                    })
+                })
+                .and_then(|_| {
+                    info!("~~~~~~~~~~~~~~~~~~~~~~~~~~~ H2 connection CLOSE !!!!!! ~~~~~~~~~~~");
+                    Ok(())
+                })
+                .then(|res| {
+                    if let Err(e) = res {
+                        error!("  -> err={:?}", e);
                     }
 
                     Ok(())
                 })
-            })
-            .and_then(|_| {
-                println!("~~~~~~~~~~~~~~~~~~~~~~~~~~~ H2 connection CLOSE !!!!!! ~~~~~~~~~~~");
-                Ok(())
-            })
-            .then(|res| {
-                if let Err(e) = res {
-                    println!("  -> err={:?}", e);
-                }
+                .with_subscriber(subscriber2.clone());
 
-                Ok(())
-            });
+            tokio::spawn(Box::new(connection));
+            Ok(())
+        })
+        .map_err(|e| error!("accept error: {}", e))
+        .with_subscriber(subscriber);
 
-        tokio::spawn(Box::new(connection));
-        Ok(())
-    })
-    .map_err(|e| eprintln!("accept error: {}", e));
-
-    tokio::run(server);
+        tokio::run(server);
+    });
 }
